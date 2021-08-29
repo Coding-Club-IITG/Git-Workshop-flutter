3822dc5fe36b58f616618e6a829249344afdb7d7
TESLA2402 <83589511+TESLA2402@users.noreply.github.com>
2021-08-29 16:17:46 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,14 +10,14 @@ const kTextFieldDecoration = InputDecoration(
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 1.0),
+    borderSide: BorderSide(color: Colors.deepOrangeAccent, width: 1.0),
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+    borderSide: BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
 );
 
 const kAnimatedTextStyle = TextStyle(
-    fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.deepPurple);
+    fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.deepOrange);
