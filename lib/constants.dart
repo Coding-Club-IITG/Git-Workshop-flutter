a5d42ac9e7f7a4a9e9e7599a71ff9ff066d6ee71
Anirudha Rajodiya <59107332+Anirudha0789@users.noreply.github.com>
2021-12-10 10:25:08 +0530

    Update constants.dart
    change in color , radius and outline width

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,17 +7,17 @@ const kTextFieldDecoration = InputDecoration(
     horizontal: 20.0,
   ),
   border: OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
   ),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 1.0),
-    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    borderSide: BorderSide(color: Colors.orangeAccent, width: 2.5),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
   ),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
-    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    borderSide: BorderSide(color: Colors.orangeAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
   ),
 );
 
 const kAnimatedTextStyle = TextStyle(
-    fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.deepPurple);
+    fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.orange);
